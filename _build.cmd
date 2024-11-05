0f8ef818794b486433f6086126fddac4832a5fd5
Henner Kollmann <henner.kollmann@gmx.de>
2024-11-05 20:20:54 +0100

    add build helper

---
+++ _build.cmd
@@ -0,0 +1,6 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+set TEMP=C:\temp
+set QTDIR=C:\Qt\5.15.2\msvc2019_64
+set QTDIR64=C:\Qt\5.15.2\msvc2019_64
+set path=C:\Qt\Tools\QtCreator\bin;%path%
+call scripts\make-release-archives.bat
